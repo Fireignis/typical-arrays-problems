1ee9184587f58ab6730310596a40db00a4c98d65
Ruslan <fire0pride@gmail.com>
2020-10-02 16:59:32 +0300

    feat: max, min, avg functions are added

--- src/index.js
+++ src/index.js
@@ -1,12 +1,43 @@
-
-exports.min = function min (array) {
+exports.min = function min(array) {
+  if (array !== undefined && array.length !== 0) {
+  let max = array[0];
+  for (let i = 1; i < array.length; i++) {
+  if (max > array[i]) {
+  max = array[i];
+  } else {
+  console.log(max);
+  }
+  }
+  return max;
+  } else {
   return 0;
-}
-
-exports.max = function max (array) {
+  }
+  };
+  
+  exports.max = function max(array) {
+  if (array !== undefined && array.length !== 0) {
+  let max = array[0];
+  for (let i = 1; i < array.length; i++) {
+  if (max < array[i]) {
+  max = array[i];
+  } else {
+  console.log(max);
+  }
+  }
+  return max;
+  } else {
   return 0;
-}
-
-exports.avg = function avg (array) {
+  }
+  };
+  
+  exports.avg = function avg(array) {
+  if (array !== undefined && array.length !== 0) {
+  let result = 0;
+  for (let i = 0; i < array.length; i++) {
+  result += array[i];
+  }
+  return result / array.length;
+  } else {
   return 0;
-}
+  }
+  };
